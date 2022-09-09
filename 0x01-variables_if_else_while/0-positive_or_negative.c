fc4546dc19a54c309ba476205e8eb58511e88292
Lord <yrnlehjend@gmail.com>
2022-09-09 21:21:15 +0000

    logical statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** 
+/**
  * main - assigns a random number to int n everytime 
  * it executes, and prints it
  * Return: Always 0 (Success)
@@ -12,7 +12,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%d is positive\n", n);	
 	else if (n == 0)
