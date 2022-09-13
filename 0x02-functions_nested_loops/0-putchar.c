365465e729e00e81dfe64ec0d3e29167ed021f49
Lord <yrnlehjend@gmail.com>
2022-09-13 13:04:01 +0000

    function to print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* This function print putchar
+ * return none
+*/
+void putcharFunction(){
+	printf("_putchar \n");
+}
+int main(void){
+	putcharFunction();
+	return (0);
+}
+
+
+		
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This repository contains functions of C
