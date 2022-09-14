8954268b6b5ff54f1c7021489a4b310d21131519
Lord <yrnlehjend@gmail.com>
2022-09-14 17:36:57 +0000

    fix betty errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- *  print_alphabet in lowercase follwed by a new line
+ *  print_alphabet - print in lowercase follwed by a new line
+ *  @ch: the character to be check
+ *  Return: no return
  */
 void print_alphabet(void)
 {
 	char ch;
+
 	ch = 'a';
 	
 	while (ch <= 'z')
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - calls other funcs
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
