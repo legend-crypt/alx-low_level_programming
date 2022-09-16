1b6b6b6e3da092b0cec4cb8fc9e53415bcf47d4f
Lord <yrnlehjend@gmail.com>
2022-09-16 15:50:04 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print sequence of numbers followed by a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+		_putchar('\n');
+	}
+}
