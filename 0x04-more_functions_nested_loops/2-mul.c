3fdc31374d127f0caa2ed8611fe42533d77426e7
Lord <yrnlehjend@gmail.com>
2022-09-16 15:36:41 +0000

    add two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: first mulplicand
+ * @b:second mulplicand
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
