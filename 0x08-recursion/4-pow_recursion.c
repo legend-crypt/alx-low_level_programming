a6fc0d13354a948754c0790f7b991abe6445c560
Lord <yrnlehjend@gmail.com>
2022-09-28 20:45:59 +0000

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,11 +15,15 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y == 0)
 	{
-		return (0);
+		return (1);
+	}
+	else if (x == 1)
+	{
+		return (1);
 	}
 	else
 	{
-		return (x * _pow_recursion((x), y - 1));
+		return (x * _pow_recursion(x, y - 1));
 
 	}
 }
