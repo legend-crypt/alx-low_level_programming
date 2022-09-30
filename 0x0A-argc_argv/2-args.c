e4dee9a7feb77393abd23def94a731cdb892be6b
Lord <yrnlehjend@gmail.com>
2022-09-30 23:29:07 +0000

    task 1,2,3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - takes arguements from the terminal
+ * @argc: the args
+ * @argv: contains a string of each argument passed
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - takes arguements from the terminal
+ * @argc: the args
+ * @argv: contains a string of each argument passed
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int product;
+
+	product = 1;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			product *= atoi(argv[i]);
+		printf("%d\n", product);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
