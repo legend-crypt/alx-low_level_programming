c7aabe82d46ee2e5372dd14bf18829eecee6003d
Lord <yrnlehjend@gmail.com>
2022-09-30 23:00:40 +0000

    fix betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,14 @@
 /**
  * main - takes arguements from the terminal
  * @argc: the args
- * @:argv: contains a string of each argument passed
+ * @argv: contains a string of each argument passed
  * Return: 0 always
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
