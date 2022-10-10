17e2dbeeaacc713946d1ab52be05bc27fa0cb350
Lord <yrnlehjend@gmail.com>
2022-10-10 14:39:31 +0000

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - create a dog struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner the dog
+ *
+ * Description: the struct give a the qualities of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
