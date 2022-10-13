2393bca3ccae6084eb29f34518ab1b5bcb7b94e8
Lord <yrnlehjend@gmail.com>
2022-10-13 23:17:03 +0000

    task 2

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - this function returns sum of all its parameters
+ * @separator: the separator
+ * @n: the guard
+ * Return: the print of the input
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list argz;
+
+	if (n > 0)
+	{
+		va_start(argz, n);
+		if (separator == NULL)
+			separator = ""
+		printf("%d", va_arg(argz, int));
+		for (i = 1; i < n; i++)
+		{
+			printf("%s%d", separator, va_arg(argz, int));
+		}
+		va_end(argz);
+		printf("\n");
+	}
+}
