f5f240405aa449110c6c6e52f225ad4260de1f55
Lord <yrnlehjend@gmail.com>
2022-10-18 23:24:25 +0000

    added final touches

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 
 #endif
