ac0149f3c23a8737d63a3266623992dec45818af
Lord <yrnlehjend@gmail.com>
2022-10-18 22:39:29 +0000

    finish task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
 #endif
