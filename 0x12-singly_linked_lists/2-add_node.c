115ae416c102f8ffcab484aea426c9aba742dbd5
Lord <yrnlehjend@gmail.com>
2022-10-20 22:44:02 +0000

    hot fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,11 +10,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+	unsigned int len = 0;
+
+	while (str != '\0')
+		len++;
 	if (new_node == NULL)
 		return NULL;
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = len;
 
 	new_node->next = *head;
 	*head = new_node;
