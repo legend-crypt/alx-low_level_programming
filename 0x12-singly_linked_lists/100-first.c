5ec0a9d1cbf0e06d80b003abaeef75a677928500
Lord <yrnlehjend@gmail.com>
2022-10-22 00:29:16 +0000

    fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,7 @@ int run_first(void) __attribute__ ((constructor));
 
 int run_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 	return (0);
 }
