797e85c5a3549a05d826056fa06be7ee3818bd8e
Lord <yrnlehjend@gmail.com>
2023-03-19 22:41:30 +0000

    add check for empty list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (*head == NULL)
 	{
-		return (NULL);
+		new_node->n = n;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
 	}
 	new_node->n = n;
 	new_node->next = *head;
