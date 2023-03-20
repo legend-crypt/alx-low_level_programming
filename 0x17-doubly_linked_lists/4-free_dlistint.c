1dc38528086abdcf4e1593075ac7efb5a288c984
Lord <yrnlehjend@gmail.com>
2023-03-20 00:42:08 +0000

    free a list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,11 +7,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp = head;
+	dlistint_t *temp;
 
-	while(temp)
+	while(head)
 	{
-		temp = temp->next;
-		free(temp);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
