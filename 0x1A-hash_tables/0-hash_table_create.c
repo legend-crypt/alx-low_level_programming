b0b9a372ad5bd8abe0aed7e0dae0c95c129765d2
Lord <yrnlehjend@gmail.com>
2023-04-07 09:26:01 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = (hash_table_t*) malloc(sizeof(hash_table_t));
 	table->size = size;
 	table->array = (hash_node_t**) malloc(table->size * sizeof(hash_node_t*));
-
+	
+	if (table == NULL)
+	{
+		return NULL;
+	}
+	if (table->array == NULL)
+	{
+		return NULL;
+	}
 	while(i < size)
 	{
 		table->array[i] = NULL;
