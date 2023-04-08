38f3786f9b5d8d298872fef1d81a5a76324ba477
Lord <yrnlehjend@gmail.com>
2023-04-08 21:52:31 +0000

    table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element to the hash table
+ * @ht:  the hash table we adding the elememt to 
+ * @key: the key of the element 
+ * @value: the value of the element
+ * Return: 1 if successful 0 if fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index(key, 100)
+	ht->
+}
