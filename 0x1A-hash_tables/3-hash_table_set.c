8b64da4d50fca3f32468497957796171e7c83004
Lord <yrnlehjend@gmail.com>
2023-04-10 12:11:12 +0000

    fix heap errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 	hash_node_t *temp;
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
@@ -18,9 +18,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-
-	strcpy(new_node->value, value);
-	strcpy(new_node->key, key);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 
 	if (ht->array[index] == NULL)
@@ -28,18 +27,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new_node;
 	}
 
-	temp = ht->array[index];
-	while (temp->next != NULL)
+	else
 	{
-		if (strcmp(temp->key, key) == 0)
+		while (temp != NULL && temp->next != NULL)
 		{
-			strcpy(new_node->value, value);
-			free(new_node);
-			break;
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(temp->value);
+                temp->value = strdup(value);
+                free(new_node->key);
+                free(new_node->value);
+                free(new_node);
+                return (1);
+			}
+			temp = temp->next;
 		}
-		temp = temp->next;
-	}
 	new_node->next = temp;
-	temp = new_node;
+	}
 	return (1);
 }
