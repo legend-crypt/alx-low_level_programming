d79898593fedc486387b567e0ea69651e7d6428d
Lord <yrnlehjend@gmail.com>
2023-04-10 11:05:16 +0000

    fix betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,8 +2,8 @@
 
 /**
  * hash_table_set - add an element to the hash table
- * @ht:  the hash table we adding the elememt to 
- * @key: the key of the element 
+ * @ht:  the hash table we adding the elememt to
+ * @key: the key of the element
  * @value: the value of the element
  * Return: 1 if successful 0 if fail
  */
@@ -13,38 +13,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	hash_node_t *temp;
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+
 	if (ht == NULL || key == NULL || new_node == NULL)
 	{
 		return (0);
 	}
 
+	strcpy(new_node->value, value);
+	strcpy(new_node->key, key);
+	new_node->next = NULL;
+
 	if (ht->array[index] == NULL)
 	{
-		strcpy(new_node->value, value);
-		strcpy(new_node->key, key);
-		new_node->next = NULL;
 		ht->array[index] = new_node;
-		free(new_node);
-		return (1);
 	}
-	if (ht->array[index] != NULL)
-	{
 
-		temp = ht->array[index];
-		strcpy(new_node->value, value);
-		strcpy(new_node->key, key);
-		while(temp->next != NULL)
+	temp = ht->array[index];
+	while (temp->next != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
 		{
-			if (strcmp(temp->key, key) == 0)
-			{
-				break;
-			}
-			temp = temp->next;
+			strcpy(new_node->value, value);
+			free(new_node);
+			break;
 		}
-		new_node->next = temp;
-		temp = new_node;
-		free(new_node);
-		return (1);
+		temp = temp->next;
 	}
+	new_node->next = temp;
+	temp = new_node;
 	return (1);
 }
