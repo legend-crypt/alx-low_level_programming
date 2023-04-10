39af829e31c41986da30a26971099f4f368bc2be
Lord <yrnlehjend@gmail.com>
2023-04-10 10:21:56 +0000

    add item to table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,6 +10,34 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index(key, 100)
-	ht->
+	hash_node_t *new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	hash_node_t *temp;
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	if (ht == NULL || key == NULL || new_node == NULL)
+	{
+		return (0);
+	}
+
+	if (ht->array[index] == NULL)
+	{
+		strcpy(new_node->value, value);
+		strcpy(new_node->key, key);
+		new_node->next = NULL;
+		ht->array[index] = new_node;
+		return (1);
+	}
+	if (ht->array[index] != NULL)
+	{
+		temp = ht->array[index];
+		while(temp != NULL)
+		{
+			temp = temp->next;
+		}
+		strcpy(new_node->value, value);
+		strcpy(new_node->key, key);
+		temp->next = new_node;
+		new_node->next = NULL;
+		return (1);
+	}
+	return (1);
 }
