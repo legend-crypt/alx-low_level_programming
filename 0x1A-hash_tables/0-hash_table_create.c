6a2b5105182af7b9f87c662cdc24ab9e7a05c9ca
KONADU LORD <82140196+legend-crypt@users.noreply.github.com>
2023-04-11 08:27:28 +0000

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	table->size = size;
-	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 
 	if (table == NULL)
 	{
