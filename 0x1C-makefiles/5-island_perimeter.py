524da7df107a021a3b9016a88714bd9189489706
¨lord¨ <yrnlehjend@gmail.com>
2023-06-11 19:30:02 +0000

    solve island question

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def island_perimeter(grid):
+    """
+        Returns the perimeter of the island in the grid
+
+        Args:
+            grid (int): list of lists
+
+        Return (int): the perimeter of and island
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
